49550cb593ff6b4cb86650169dff5ccb973c1251
Blessed Sode <emi.square23@gmail.com>
2025-07-21 21:49:46 +0100

    updated

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import Author, Library, Librarian, CustomUser
 
+# Register your models here.
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role', 'date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('role', 'date_of_birth', 'profile_photo')}),
+    )
+    list_display = ['username', 'email', 'role', 'date_of_birth', 'is_staff']
+    search_fields = ['username', 'email', 'role']
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
@@ -10,6 +23,10 @@ class BookAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Author)
+admin.site.register(Library)
+admin.site.register(Librarian)
 
 
 
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -16,6 +16,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=250)
     author = models.CharField(max_length=150)
+    publication_year = models.IntegerField()
 
     def __str__(self):
         return self.title
