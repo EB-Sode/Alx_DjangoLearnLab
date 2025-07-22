56852b59716e7582b4d7b29aba12d5d4a8a2104a
Blessed Sode <emi.square23@gmail.com>
2025-07-22 23:40:27 +0100

    update

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,17 +4,18 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView as DjangoLoginView
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.decorators import permission_required
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm 
+
 
 # Create your views here.
 @permission_required('bookshelf.can_view_books')
 def book_list(request):
     books= Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, 'bookshelf/list_books.html', {'books': books})
 
 @permission_required('bookshelf.can_create_books', raise_exception= True)
 def create_book(request):
@@ -26,8 +27,8 @@ def create_book(request):
             return redirect('list_books')  # Adjust to your book list view name
         else:
             error = "Both title and author are required."
-            return render(request, 'relationship_app/add_book.html', {'error': error})
-    return render(request, 'relationship_app/add_book.html')
+            return render(request, 'bookshelf/add_book.html', {'error': error})
+    return render(request, 'bookshelf/add_book.html')
 
 @permission_required('bookshelf.can_edit_books', raise_exception=True)
 def edit_book(request, book_id):
@@ -42,8 +43,8 @@ def edit_book(request, book_id):
             return redirect('list_books')
         else:
             error = "Both title and author are required."
-            return render(request, 'relationship_app/add_book.html', {'error': error})
-    return render(request, 'relationship_app/edit_book.html', {'book': book})
+            return render(request, 'bookshelf/add_book.html', {'error': error})
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
 
 @permission_required('bookshelf.can_delete_books')
 def delete_book(request, book_id):
@@ -51,20 +52,20 @@ def delete_book(request, book_id):
     if request.method == 'POST':
         book.delete()
         return redirect('list_books')
-    return render(request, 'relationship_app/delete_book.html', {'book': book})
+    return render(request, 'bookshelf/delete_book.html', {'book': book})
 
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'bookshelf/library_detail.html'
 
 class LoginView(DjangoLoginView):
     form_class = AuthenticationForm
     success_url = reverse_lazy('register')
-    template_name = 'relationship_app/login.html'
+    template_name = 'bookshelf/login.html'
 
 class LogoutView(auth_views.LogoutView):
-    template_name = 'relationship_app/logout.html'
+    template_name = 'bookshelf/logout.html'
 
 
 # class Register(CreateView):
@@ -77,15 +78,16 @@ class LogoutView(auth_views.LogoutView):
 #         login(self.request, user)  # Log the user in after registration
 #         return super().form_valid(form)
     
+
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)  # Log the user in after registration
-            return redirect('bookshelf:list_books')  # Redirect to the book list after registration
+            return redirect('bookshelf:list_books')  # Redirect after registration
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'bookshelf/register.html', {'form': form})
 
 def is_admin(user):
@@ -122,13 +124,13 @@ def is_member(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
+    return render(request, 'bookshelf/admin_view.html')
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
+    return render(request, 'bookshelf/librarian_view.html')
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
+    return render(request, 'bookshelf/member_view.html')
 
