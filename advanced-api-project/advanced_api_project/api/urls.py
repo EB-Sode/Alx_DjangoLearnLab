0223cfb70e858cad1ff8ab90d322cdff4bccdb81
Blessed Sode <emi.square23@gmail.com>
2025-08-10 22:45:45 +0100

    drf updated

--- advanced-api-project/advanced_api_project/api/urls.py
+++ advanced-api-project/advanced_api_project/api/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 from .views import BookViewSet
 from rest_framework.routers import DefaultRouter
+from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 #routers for the CRUD operations on books
-router = DefaultRouter()
-router.register(r'books_all', BookViewSet, basename='books_all')
+# router = DefaultRouter()
+# router.register(r'books_all', BookViewSet, basename='books_all')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+    path('books/', ListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', DetailView.as_view(), name='book-detail'),
+    path('books/create/', CreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', UpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', DeleteView.as_view(), name='book-delete'),
 ]
--- advanced-api-project/advanced_api_project/api/views.py
+++ advanced-api-project/advanced_api_project/api/views.py
@@ -3,13 +3,64 @@ from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
-# from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.exceptions import ValidationError
+from rest_framework import permissions
 from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
-#This viewset will handle all CRUD operations
-class BookViewSet(viewsets.ModelViewSet):
+
+#view for listing all books
+class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated, IsAdminUser]
+
+
+#view for retrieving a single book
+class DetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication]
+
+#view for creating a new book
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]  # Only logged-in users can create
+
+    def perform_create(self, serializer):
+        # Example: Prevent duplicate book titles for the same author
+        title = self.request.data.get('title')
+        author_id = self.request.data.get('author')
+        
+        if Book.objects.filter(title=title, author_id=author_id).exists():
+            raise ValidationError({"error": "This author already has a book with that title."})
+        
+
+#update the a book list
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication] 
+    permission_classes = [IsAuthenticated]  # Only logged-in users can update
+
+    def perform_update(self, serializer):
+        # Example: Prevent updating to a duplicate book title for the same author
+        title = self.request.data.get('title')
+        author_id = self.request.data.get('author')
+        book_id = self.kwargs.get('pk')
+
+        if Book.objects.filter(title=title, author_id=author_id).exclude(id=book_id).exists():
+            raise ValidationError({"error": "This author already has a book with that title."})
+        serializer.save()
+
+
+
+#delete a book
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]  # Only logged-in users can delete
