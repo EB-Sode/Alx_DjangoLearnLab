ecc9fbf890733b76a7e98a858bd1ae510f15a7eb
Blessed Sode <emi.square23@gmail.com>
2025-08-19 21:04:35 +0100

    added likes, unlike functionality and notifications

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -12,6 +12,6 @@ router.register(r"comment", CommentViewSet, basename='comments')
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', FeedView.as_view(), name="feed"), 
-    path("<int:post_id>/like/", LikePostView.as_view(), name="like-post"),
-    path("<int:post_id>/unlike/", UnlikePostView.as_view(), name="unlike-post"),
+    path("<int:pk>/like/", LikePostView.as_view(), name="like-post"),
+    path("<int:pk>/unlike/", UnlikePostView.as_view(), name="unlike-post"),
 ]
